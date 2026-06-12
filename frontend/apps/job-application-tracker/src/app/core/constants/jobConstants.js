174f9725680e1e13a8797672942ac46f9c8f3b59
sakshi <psakshi989@gmail.com>
2026-06-12 17:31:32 +0530

    Change backend URL

--- frontend/apps/job-application-tracker/src/app/core/constants/jobConstants.js
+++ frontend/apps/job-application-tracker/src/app/core/constants/jobConstants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://job-tracker-zsha.onrender.com";
 export const JOBS_ENDPOINT = `${API_BASE_URL}/jobs`;
 
 export const JOB_STATUS = {
